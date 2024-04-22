3560438c073aa1d4750867d881f7dee4c9dd783b
talented_vicky <victorotubure7@gmail.com>
2024-04-22 17:18:23 +0100

    updated ui using useState react hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const msgs = [
   "Atomic habits",
   "You won't always feel the vibe",
@@ -5,18 +7,46 @@ const msgs = [
 ]
 
 export default function App() {
+  const [steps, setSteps] = useState(1);
+  // because useState() returns an array
+
+  function decSteps() {
+    if(steps === 1){
+      return alert('No previous');
+    }
+    setSteps(steps - 1 )
+  }
+  
+  function incSteps() {
+    if(steps === msgs.length){
+      return alert('End of steps');
+    }
+    setSteps(steps + 1)
+  }
+
   return <div className="steps">
     <div className="numbers">
-      <div className="active"> 1 </div>
-      <div> 2 </div>
-      <div> 3 </div>
+      <div className={steps === 1 ? 'active' : ''}> 1 </div>
+      <div className={steps === 2 ? 'active' : ''}> 2 </div>
+      <div className={steps === 3 ? 'active' : ''}> 3 </div>
     </div>
 
-    <p className="msg"> {msgs[0]} </p>
+    <p className="msg"> {msgs[steps - 1]} </p>
 
     <div className="btns">
-      <button style={{color: "#fff", backgroundColor: "#7950f2"}}> Previous </button>
-      <button style={{color: "#fff", backgroundColor: "#7950f2"}}> Next </button>
+      <button
+        style={{color: "#fff", backgroundColor: "#7950f2"}}
+        onClick={ decSteps }
+      > 
+        Previous 
+      </button>
+
+      <button 
+        style={{color: "#fff", backgroundColor: "#7950f2"}}
+        onClick={ incSteps }
+      > 
+        Next 
+      </button>
     </div>
   </div>
 }
